29ddba9045456dad97a0e2e803f9bd0afb9d9c5b
mykels <micha1991g@gmail.com>
2018-04-10 20:16:11 +0300

    1. added messages to store 2. added threads to store

--- src/app/modules/thread/components/thread-details/thread-details.component.ts
+++ src/app/modules/thread/components/thread-details/thread-details.component.ts
@@ -1,9 +1,11 @@
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {AppState} from '../../../core/store/types/app-state';
 import {Store} from '@ngrx/store';
 import {Thread} from '../../types/thread';
 import {UserService} from '../../../user/services/user.service';
+
+// TODO: make thread-details part of threads components instead of an outlined one
 
 @Component({
   selector: 'cb-thread-details',
@@ -12,7 +14,7 @@ import {UserService} from '../../../user/services/user.service';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ThreadDetailsComponent implements OnInit {
-  thread: Thread;
+  @Input() thread: Thread;
 
   constructor(private store: Store<AppState>,
               private route: ActivatedRoute,
@@ -24,8 +26,10 @@ export class ThreadDetailsComponent implements OnInit {
     this.initThread();
   }
 
+  // TODO: make thread-details part of threads components instead of an outlined one
+
   private initThread() {
-    this.route.params.subscribe(routeParams => {
+    ---this.route.params.subscribe(routeParams => {
       const threadId = +routeParams.id;
       this.store.select('threads').subscribe(threads => {
         const [thread] = threads.filter(scannedThread => scannedThread.id === threadId);
